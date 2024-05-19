77b18b29794846e7f077c3914d1eeae79ff84b0d
Hai000 <21130343@st.hcmuaf.edu.vn>
2024-05-19 14:01:40 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch} from "react-redux";
+import ProductList from "./component/ProductList";
+import {loadProduct} from "./store/Action";
+import {products} from "./data/ProductData";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadProduct(products));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <ProductList/>
+      </div>
   );
 }
 
